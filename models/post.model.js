d83c5c56986cd6199e6580d38c39309902b15bb4
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-11 19:03:19 +0530

    feat: post model update

--- models/post.model.js
+++ models/post.model.js
@@ -13,9 +13,10 @@ const postSchema = new mongoose.Schema(
       required: true,
     },
     likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   },
   { timestamps: true }
 );
 
 const Post = mongoose.model("posts", postSchema);
-module.exports = { Post };
+module.exports = Post;
