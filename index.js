bdb2e579e1e97ec7f9934e0c8204388bef238e5a
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-20 19:18:00 +0530

    feat: added routers to app

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const port = process.env.PORT || 8080;
 const express = require("express");
 const cors = require("cors");
 const { initializeDBConnection } = require("./config/db.config");
-
+const userRouter = require("./routers/user.router");
+const postRouter = require("./routers/post.router");
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // called before any route
 initializeDBConnection();
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
   return res.send({ message: "Welcome" });
