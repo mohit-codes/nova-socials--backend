fe154c4ee87bc22dfbb18818d20a46997ee4b480
mohit kushwaha <kushwahamohit000@gmail.com>
2021-11-20 19:20:47 +0530

    feat: new routes

--- routers/post.router.js
+++ routers/post.router.js
@@ -7,14 +7,20 @@ const {
   deletePost,
   unlikePost,
   fetchLikes,
+  fetchSinglePost,
+  updatePost,
+  deleteComment,
   fetchComments,
 } = require("../controllers/post.controller");
 
 router.route("/new").post(createPost);
 router.route("/like").post(likePost);
-router.route("/comment").post(commentPost);
 router.route("/unlike").post(unlikePost);
+router.route("/comment").post(commentPost);
+router.route("/comment/:commentId").delete(deleteComment);
+router.route("/:postId").get(fetchSinglePost);
 router.route("/likes/:postId").get(fetchLikes);
+router.route("/update-post").put(updatePost);
 router.route("/comments/:postId").get(fetchComments);
 router.route("/delete/:postId").delete(deletePost);
 
