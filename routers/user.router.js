1b2c4110c4630c75a5644aa711cd09345d505dfa
mohit kushwaha <kushwahamohit000@gmail.com>
2021-12-03 18:56:13 +0530

    feat: new route added and previous route changes

--- routers/user.router.js
+++ routers/user.router.js
@@ -11,23 +11,29 @@ const {
   updateCurrentUserDetails,
   fetchUserPosts,
   follow,
+  unFollow,
   getUserFeed,
   fetchUserFollowers,
   fetchUserFollowing,
   getSingleUserInfo,
+  fetchRecentlyJoinedUsers,
 } = require("../controllers/user.controllers");
 
 router.route("/login").post(login);
 router.route("/signup").post(signup);
 router.route("/follow").post(follow);
+router.route("/unfollow").post(unFollow);
 
 router.param("userId", searchById);
 router.route("/:userId").get(getSingleUserInfo);
 router.route("/feed/:userId").get(getUserFeed);
-router.route("/followers/:userId").get(fetchUserFollowers);
-router.route("/following/:userId").get(fetchUserFollowing);
-router.route("/get-user-posts/:userId").get(fetchUserPosts);
+router.route("/followers").post(fetchUserFollowers);
+router.route("/following").post(fetchUserFollowing);
+router.route("/get-user-posts").post(fetchUserPosts);
 router.route("/update/:userId").put(updateCurrentUserDetails);
 router.route("/notifications/:userId").get(fetchUserNotifications);
+router
+  .route("/get-recently-joined-users/:userId")
+  .get(fetchRecentlyJoinedUsers);
 
 module.exports = router;
