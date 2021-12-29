15265d1563a51e5e87d1b8e39fceb8c14b9210fd
mohit kushwaha <kushwahamohit000@gmail.com>
2021-12-29 18:48:29 +0530

    feat: adds delete chat route

--- routers/message.router.js
+++ routers/message.router.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   getMessages,
   deleteMessageById,
+  deleteChatByRecipientId,
 } = require("../controllers/message.controller");
 
+router.route("/delete-chat").post(deleteChatByRecipientId);
 router.route("/get_messages").post(getMessages);
 router.route("/:messageId").delete(deleteMessageById);
 
